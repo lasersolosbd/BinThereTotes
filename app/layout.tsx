ce8438d4d088e1b0daeeda8e5a6e4212b3c197a6
lasersolosbd <mark@solobusinessdude.com>
2026-05-24 20:30:53 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,8 +49,16 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-}
-
+icons: {
+  icon: [
+    { url: '/favicon.ico' },
+    { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
+  ],
+  apple: [
+    { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+  ],
+},
 export default function RootLayout({
   children,
 }: {
